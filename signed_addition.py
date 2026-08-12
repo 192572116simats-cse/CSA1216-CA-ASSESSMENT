f8144eb6d3159a1ce86e87f0c0b1be001cd4c6d4
192572116simats-cse <192572116.simats@saveetha.com>
2026-08-12 22:37:29 +0530

    Update signed_addition.py

--- signed_addition.py
+++ signed_addition.py
@@ -1,1 +1,24 @@
+def add(a, b):
+    bits = 8
+    mask = 255
 
+    A = a & mask
+    B = b & mask
+    R = (A + B) & mask
+
+    print("A :", format(A, "08b"))
+    print("B :", format(B, "08b"))
+    print("R :", format(R, "08b"))
+
+    overflow = ((A ^ R) & (B ^ R) & 128) != 0
+
+    if R & 128:
+        result = R - 256
+    else:
+        result = R
+
+    print("Result:", result)
+    print("Overflow:", overflow)
+
+
+add(120, 50)
